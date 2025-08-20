e55951f05f3b0ec859d3ab03b9787a738a024380
alokbhanawat <alokbhanawat@gmail.com>
2025-08-20 21:08:54 +0530

    Read Program

---
+++ src/Codes/Read.java
@@ -0,0 +1,25 @@
+package Codes;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Read {
+
+	public static void main(String[] args) {
+          int num;
+          
+          
+          System.out.println("Enter The Integer");
+          
+          Scanner s = new Scanner(System.in);
+          
+          num = s.nextInt();
+          
+          System.out.println("Enter Integer is :"+num);
+          
+          s.close();
+
+          
+   }
+
+}
