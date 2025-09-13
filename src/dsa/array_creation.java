edc3bcdac81940f96bf7657da248d8e3b19ad19d
alokbhanawat <alokbhanawat@gmail.com>
2025-09-13 14:13:43 +0530

    Array Creation

---
+++ src/dsa/array_creation.java
@@ -0,0 +1,45 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class array_creation {
+	public static void main(String args[]) {
+		 int n;
+		 Scanner s = new Scanner(System.in);
+		 System.out.println("Enter No Of Elements in Array");
+		 n=s.nextInt();
+		 int a[]=new int[n];
+		 int b[]=new int[n+1];
+
+		 System.out.println("Enter Values");
+		 for (int i=0;i<n;i++) {
+			 a[i]=s.nextInt();
+		 }
+		 
+		 System.out.println("Enter Index of new values to be Inserted ");
+         int m=s.nextInt();	
+         System.out.println("Ente new values to be Inserted ");
+         int p=s.nextInt();
+         for (int i=0; i<n+1;i++) {
+        	       if (i<m) {
+        	    	     b[i]=a[i];
+        	       }
+        	       else if(i==m) {
+        	    	    b[i]=p;
+        	       }
+        	       else {
+        	    	   b[i]=a[i-1];
+        	       }
+        	 
+         }
+		 
+		 
+		 System.out.println("Elements are : ");
+		 for (int i=0;i<n+1;i++) {
+			System.out.println(b[i]);
+		 }
+
+		 
+	}
+
+}
