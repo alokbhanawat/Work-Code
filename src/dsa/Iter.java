2a049cf94ea58fe5d184feabe461e5cf3efac9a6
alokbhanawat <alokbhanawat@gmail.com>
2025-09-29 19:46:45 +0530

    List Iterator

---
+++ src/dsa/Iter.java
@@ -0,0 +1,32 @@
+package dsa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+
+
+public class Iter {
+
+	public static void main(String[] args) {
+		
+		List <Integer> numbers = new ArrayList<Integer>
+();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		
+		ListIterator<Integer> item =numbers.listIterator();
+		System.out.println("Displaying numbers...");
+		
+		while (item.hasNext()) {
+			System.out.println(item.next());
+		}
+		System.out.println("Display numbers in the reverse order");
+		while (item.hasPrevious()) {
+			System.out.println(item.previous());
+	}
+	}
+
+}
