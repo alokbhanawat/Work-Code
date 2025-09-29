568bfea922250f57f803fa8b321a9c65aa50195d
alokbhanawat <alokbhanawat@gmail.com>
2025-09-29 19:46:22 +0530

    Array List

---
+++ src/dsa/Arl1.java
@@ -0,0 +1,15 @@
+package dsa;
+
+import java.util.ArrayList; 
+import java.util.List;
+
+class Arl1 {
+	public static void main(String[] args) {
+		List<String> food = new ArrayList<String>(); 
+		food.add("Pizza"); // Adding elements
+		food.add("Burger");
+		food.add("Pasta");
+		food.add("Sandwich");
+		System.out.println("Food items: " + food);
+	}
+}
